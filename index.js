14b2e5fd8b97a3a2771f8e5a5c0ea085978bed41
Robert Laws <roblaws@me.com>
2018-04-07 09:59:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ class Driver {
 
   yearsExperienceFromBeginningOf(year) {
     var beginning = new Date(year, 1, 1, 0, 0, 0);
-    return (beginning - this.startDate) / (1000*60*60*24*365);
+    return Math.ceil((beginning - this.startDate) / (1000*60*60*24*365));
   }
 }
