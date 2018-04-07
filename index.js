5a3d8afc3cc28d28f98221f11e338d9d76a2afec
Robert Laws <roblaws@me.com>
2018-04-07 10:05:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,11 +21,13 @@ class Route {
   avenueToInteger(avenue){
     return eastWest.indexOf(avenue)
   }
+  
   blocksTravelled(){
     let horizontalDistance = this.avenueToInteger(this.endingLocation.horizontal) - this.avenueToInteger(this.beginningLocation.horizontal)
     let verticalDistance = this.endingLocation.vertical - this.beginningLocation.vertical
     return (Math.abs(horizontalDistance) + Math.abs(verticalDistance))
   }
+  
   estimatedTime(peak){
     if(peak){
       return this.blocksTravelled()/2
